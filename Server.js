fb01a99cc5928a1c4f9a516412edb0e067eae29b
solarflurd-source <solarflurd@gmail.com>
2026-07-22 20:19:30 +0200

    Create Server.js

---
+++ Server.js
@@ -0,0 +1,13 @@
+{
+  "name": "tiktok-game",
+  "version": "1.0.0",
+  "description": "TikTok Live Interactive Game",
+  "scripts": {
+    "start": "node server.js"
+  },
+  "dependencies": {
+    "express": "^4.18.2",
+    "socket.io": "^4.7.2",
+    "tiktok-live-connector": "^1.1.1"
+  }
+}
